7f7408bfd288033d456b73197849b1ed80236e97
esteban.araya <esteban.araya@domo.com>
2012-05-23 17:06:49 -0600

    moved src folder. added js build

---
+++ build/build.js
@@ -0,0 +1,9 @@
+{
+	baseUrl: '../src/js',
+	out: '../public/js/app/main.js',
+	mainConfigFile: '../src/js/main.js',
+	optimize: 'uglify',
+	modules: [{ name: 'main' }],
+	paths: { jquery: 'empty:' }
+}
+	
---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+define(function() {
+	var app = {
+		name: 'name'
+	};
+	return app;
+});
