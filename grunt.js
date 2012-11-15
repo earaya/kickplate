f8a8af8774490f45a3823011108ad847e17595ae
esteban.araya <esteban.araya@domo.com>
2012-11-14 17:23:20 -0700

    added grunt file w/ linting

---
+++ grunt.js
@@ -0,0 +1,39 @@
+/*global module:false*/
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    lint: {
+      files: ['grunt.js', 'www/js/app/*.js', 'test/**/*.js']
+    },
+    test: {
+      files: ['test/**/*.js']
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'lint test'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true
+      },
+      globals: { 
+        define: true,
+        requirejs: true
+      }
+    }
+  });
+
+  // Default task.
+  grunt.registerTask('default', 'lint test');
+
+};
