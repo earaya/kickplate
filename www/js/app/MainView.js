41c8052dd1af1568da876d035f58fe95b0b1f057
Esteban Araya <esteban.araya@gmail.com>
2013-01-04 21:40:57 -0700

    Remove LESS support.

--- www/js/app/MainView.js
+++ www/js/app/MainView.js
@@ -1,7 +1,6 @@
 define(function(require) {
     var Marionette = require('marionette');
     var tmpl = require('hbs!./main-view-template');
-    var styl = require('less!./main-style');
 
     return Marionette.ItemView.extend({
         template: tmpl
